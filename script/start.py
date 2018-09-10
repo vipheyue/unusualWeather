097a32f3cf32884aa7f691a0ef37461823785077
何悦 <vipheyue@foxmail.com>
2018-09-10 11:18:22 +0800

    tt

--- script/start.py
+++ script/start.py
@@ -1,12 +1,19 @@
+#!/bin/python3
 import os
 
 import time
 
 
 def start_web_app():
+    os.system("echo start_web_app---")
     os.system("cd /opt/unusualWeather/")
-    os.system(". venv/bin/activate")
-    os.system("uwsgi uwsgi.ini")
+    os.system("git fetch —a ")
+    os.system("git fetch origin master")
+    os.system("git reset --hard origin/master ")
+    os.system("echo ---end---")
+
+    # os.system(". venv/bin/activate")
+    # os.system("uwsgi uwsgi.ini")
     # time.sleep(1)
 
 

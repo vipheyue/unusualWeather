0626e8d7488c058c50bd2677670e5b6063980572
vipheyue <vipheyue@foxmail.com>
2018-09-16 14:10:46 +0800

    定时任务

--- app/run.py
+++ app/run.py
@@ -44,12 +44,48 @@ def show_user_profile(username):
     return 'User %s' % username
 
 
+@app.route('/scheduler/start')
+def scheduler_start():
+    from app.task.task_manager import g_scheduler
+    g_scheduler.start()
+    return 'g_scheduler.start()'
+
+
+@app.route('/scheduler/startJob')
+def scheduler_startJob():
+    from app.task.task_manager import g_scheduler
+    from app.task.task_manager import add_job
+    add_job()
+    scheduler_start()
+    return 'g_scheduler.startJob() scheduler_start'
+
+
+@app.route('/scheduler/print_jobs')
+def test2():
+    from app.task.task_manager import g_scheduler
+    g_scheduler.print_jobs()
+    return 'print_jobs'
+
+
+@app.route('/scheduler/get_jobs')
+def get_jobs():
+    from app.task.task_manager import get_jobs
+    return get_jobs()
+
+
+@app.route('/scheduler/shutdown')
+def test3():
+    from app.task.task_manager import g_scheduler
+    g_scheduler.shutdown()
+    return 'shutdown'
+
+
 def application(env, start_response):
     str(env)
-    start_response('200 OK', [('Content-Type','text/html')])
+    start_response('200 OK', [('Content-Type', 'text/html')])
     return [b"Hello World------------------"]
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-
     # schedule_task()
---
+++ app/task/task_manager.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
+import time
+from app.notice.mail import send_email
+from app.weather.caiyun import check_unuaual_weather, realtime
+
+g_scheduler = BackgroundScheduler()
+
+
+# g_scheduler = BlockingScheduler()
+def job1():
+    print("%s: 执行任务" % time.asctime())
+
+
+def job():
+    # 121.6544,25.1552
+
+    # longitude = 116.298056  # 经度-何悦
+    # latitude = 39.959912  # 纬度
+    longitude = 121.6544  # 经度
+    latitude = 25.1552  # 纬度
+    # realtime(longitude, latitude)
+    result = check_unuaual_weather(longitude, latitude)
+    if result != '':
+        # 发送邮件
+        send_email("vipheyue@foxmail.com", result)
+
+
+def dailyWeather():
+    longitude = 116.298056  # 经度-何悦
+    latitude = 39.959912  # 纬度
+    result = realtime(longitude, latitude)
+    send_email("vipheyue@foxmail.com", result)
+
+
+def add_job():
+    # g_scheduler.add_job(job1, 'date', run_date=datetime(2018, 9, 16, 12, 30, 10), args=[])
+    # g_scheduler.add_job(job1, 'interval', seconds=1)
+    # g_scheduler.add_job(dailyWeather, 'cron', hour=13, minute=14)
+    g_scheduler.add_job(dailyWeather, 'cron', hour=7, minute=25)
+    g_scheduler.add_job(dailyWeather, 'cron', hour=18, minute=0)
+    g_scheduler.print_jobs()
+
+def get_jobs():
+    jobs = g_scheduler.get_jobs()
+    result = ""
+    for job in jobs:
+        result += str(job)+"\n"
+    # print(result)
+    return result
+
+
+if __name__ == '__main__':
+    add_job()
+    get_jobs()
+    # dailyWeather()

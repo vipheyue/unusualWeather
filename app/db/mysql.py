8ca544ade1060653180955269087f3691b78daa6
vipheyue <vipheyue@foxmail.com>
2018-09-21 21:39:34 +0800

    数据库测试

--- app/db/mysql.py
+++ app/db/mysql.py
@@ -1,15 +1,18 @@
 import pymysql.cursors
 
 # Connect to the database
-connection = pymysql.connect(host='localhost',
+connection = pymysql.connect(host='ali.welightworld.com',
                              user='root',
                              password='dockermysql',
-                             db='db',
+                             db='test',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
 try:
     with connection.cursor() as cursor:
+        cursor.execute("SELECT VERSION()")
+        data = cursor.fetchone()
+        print("Database version : %s " % data)
         # Create a new record
         sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
         cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
@@ -24,5 +27,17 @@ try:
         cursor.execute(sql, ('webmaster@python.org',))
         result = cursor.fetchone()
         print(result)
+
+        # 使用预处理语句创建表
+        # sql = """CREATE TABLE EMPLOYEE (
+        #          FIRST_NAME  CHAR(20) NOT NULL,
+        #          LAST_NAME  CHAR(20),
+        #          AGE INT,
+        #          SEX CHAR(1),
+        #          INCOME FLOAT )"""
+        # cursor.execute(sql)
 finally:
     connection.close()
+
+
+

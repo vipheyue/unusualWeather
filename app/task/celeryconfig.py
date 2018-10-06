5d264ab65dc7e721f5676339f5268ad761d456ab
vipheyue <vipheyue@foxmail.com>
2018-10-06 15:02:43 +0800

    ....

--- app/task/celeryconfig.py
+++ app/task/celeryconfig.py
@@ -1,9 +1,9 @@
 import flower
 from celery.schedules import crontab
-# broker_url = 'redis://s.welightworld.com:6379/0'
-# result_backend = 'redis://s.welightworld.com:6379/1'
-broker_url = 'redis://localhost:6379/0'
-result_backend = 'redis://localhost:6379/1'
+broker_url = 'redis://s.welightworld.com:6379/0'
+result_backend = 'redis://s.welightworld.com:6379/1'
+# broker_url = 'redis://localhost:6379/0'
+# result_backend = 'redis://localhost:6379/1'
 
 # task_serializer = 'json'
 # result_serializer = 'json'
